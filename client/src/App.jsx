72aeff6888734ecb39c90e49eb9d52294429ce13
Elvin <elvinnadir92@gmail.com>
2023-04-14 16:56:21 +0400

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Canvas from './canvas/index';
+import Home from './pages/Home';
+import Customizer from './pages/Customizer';
+
+
+const App = () => {
+  return (
+    <main className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
+  )
+}
+
+export default App
